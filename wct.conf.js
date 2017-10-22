f674c2450931f21b4cd29f179b32703477c7b65f
Prem Parihar <loveprem073@gmail.com>
2017-10-22 21:53:05 +0530

    t-hotel-search-api test

--- wct.conf.js
+++ wct.conf.js
@@ -4,7 +4,8 @@ module.exports = {
         dir: "./test/coverage-report",
         reporters: ["text-summary", "lcov"],
         include: [
-          "/t-component-api/t-component-api.html"
+          "/t-component-api/t-component-api.html",
+          "/t-component-api/t-hotel-search-api.html"
         ],
         exclude: [
           "/polymer/polymer.js",
